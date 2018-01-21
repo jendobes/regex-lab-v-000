c5dab247cc5fe7ca40b9f052273e7e8962f31818
Jennifer Dobak <jdobak1@gmail.com>
2018-01-21 21:41:24 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,9 +15,12 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].+$[\.!?]/)
+    return true
+  else 
+    false
 end
 
 def valid_phone_number?(phone)
-
+  
 end

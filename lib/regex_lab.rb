3277f07ba126f00e03a2f37e75771a8179c36a59
Jennifer Dobak <jdobak1@gmail.com>
2018-01-21 21:41:44 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,10 +17,11 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/^[A-Z].+$[\.!?]/)
     return true
-  else 
+  else
     false
+  end
 end
 
 def valid_phone_number?(phone)
-  
+
 end
